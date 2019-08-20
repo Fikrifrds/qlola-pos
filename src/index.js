9a11aace3e9c90e7678e1bcc44b6c9bc3d62e8cf
Fikri Firdaus <fikri.firdaus26@gmail.com>
2019-08-20 20:15:44 +0700

    better

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import MediaState from './context/MediaQueryContextProvider';
 import './index.css';
 import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
  <MediaState>
@@ -15,3 +16,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.register();
